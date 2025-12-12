791161502dcb8a3fa5053de7fdbbd773f752205b
kipper.luis <31864582+kipperpqd@users.noreply.github.com>
2025-12-12 13:20:35 -0300

    Add EMBEDDING_DIMENSION to Settings class

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,18 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     # ... Outras configurações (Supabase, LLM Keys) ...
     EMBEDDING_MODEL_NAME: str
+    EMBEDDING_DIMENSION: str
     GENERATION_MODEL_NAME: str
+    LLM_API_KEY: str
+
+    # URL da sua instância Supabase (Coolify ou serviço externo)
+    SUPABASE_URL: str
+    # Chave de serviço (service_role key) para o backend, para acesso total (Ingestão)
+    SUPABASE_SERVICE_KEY: str
+    # Nome da tabela onde os chunks e embeddings serão armazenados
+    SUPABASE_TABLE_NAME: str
+
+
     # --- Configurações do Google Drive ---
     GOOGLE_CLIENT_ID: str
     GOOGLE_CLIENT_SECRET: str
